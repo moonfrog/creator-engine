1308942e7f14c9744681a9b62158b4ca2401b3a5
shreyas-prasad <shreyas.prasad@moonfroglabs.com>
2018-08-28 16:27:34 +0530

    adding font to edibox

--- cocos2d/core/components/CCEditBox.js
+++ cocos2d/core/components/CCEditBox.js
@@ -374,6 +374,23 @@ var EditBox = cc.Class({
             }
         },
 
+         /**
+         * !#en Font family of the EditBox text.
+         * !#zh 输入框的字体名称
+         * @property {String} fontFamily
+         */
+        fontFamily: {
+            default: "Arial",
+            tooltip: CC_DEV && 'i18n:COMPONENT.editbox.font_family',
+            notify: function () {
+                if (this._sgNode) {
+                    this._sgNode.fontName = this.fontFamily;
+                    this._sgNode.placeholderFontName = this.fontFamily;
+                }
+            },
+            animatable: false
+        },
+
         _tabIndex: 0,
 
         /**
@@ -492,9 +509,11 @@ var EditBox = cc.Class({
         sgNode.maxLength = this.maxLength;
 
         sgNode.string = this._string;
+        sgNode.fontName = this.fontFamily;
         sgNode.fontSize = this.fontSize;
         sgNode.fontColor = this.fontColor;
         sgNode.placeholder = this.placeholder;
+        sgNode.placeholderFontName = this.fontFamily;
         sgNode.placeholderFontSize = this.placeholderFontSize;
         sgNode.placeholderFontColor = this.placeholderFontColor;
         sgNode.inputFlag = this.inputFlag;
